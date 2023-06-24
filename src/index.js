0801f88b5f9db21753d0fd0ac3df41f0747b648a
Noah <noahhughesph@gmail.com>
2023-06-23 23:47:16 -0400

    done with delete button

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener('DOMContentLoaded', () => {
+  let form = document.getElementById('create-task-form');
+  form.addEventListener('submit', (e) => {
+    let formInput = document.getElementById('new-task-description');
+    e.preventDefault();
+    createToDoItem(formInput.value);
+  });
 });
+
+const createToDoItem = (toDoText) => {
+  let toDoList = document.querySelector('#tasks');
+  let li = document.createElement('li');
+  let btn = document.createElement('button');
+  btn.addEventListener('click', handleDelete);
+  btn.textContent = 'x';
+  li.innerText = `${toDoText} `;
+  li.appendChild(btn);
+  toDoList.appendChild(li);
+};
+
+const handleDelete = (e) => {
+  e.target.parentNode.remove();
+};
